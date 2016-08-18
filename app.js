103ab12127e34b64da88f462aef6d993ff137d9f
Tim Stallmann <tim@stallmann.mayfirst.org>
2016-08-18 11:57:39 -0400

    Add helmet and move configuration into separate conf.js file

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+var helmet = require('helmet');
+app.use(helmet());
+
+var fs = require('fs');
+eval(fs.readFileSync('conf.js', encoding="ascii"));
+
 Array.prototype.shuffle = function() {
     var i = this.length, j, temp;
     if ( i == 0 ) return this;
@@ -15,15 +21,22 @@ Array.prototype.shuffle = function() {
 
 app.get('/shuffle', function (req, res) {
     console.log(req.query);
-    if (req.query.token == 'm6fga9JMwDemJbZhhhfRxq28') {
+    if (req.query.token == conf.token) {
         var reply = {};
         if (req.query.channel_name != 'monday-meeting') {
             reply.text = "Wrong channel!";
         }
         else {
             // Shuffle team members
-            var absent = req.query.text.toLowerCase().split(' ');
-            var team = new Array('Anne', 'Chris', 'Dan', 'Kosta', 'Lisa', 'Oksana', 'Ro', 'Yonas');
+            var absent = new Array();
+            if (req.query.text.indexOf(',') < 0) {
+                absent = req.query.text.toLowerCase().split(' ');
+            }
+            else {
+                absent = req.query.text.toLowerCase().split(',');
+            }
+            var team = conf.team;
+            reply.response_type = "in_channel";
             reply.text = team.filter(function(i) {
                 return absent.indexOf(i.toLowerCase()) < 0;
             }).shuffle().join(", ");
@@ -35,6 +48,6 @@ app.get('/shuffle', function (req, res) {
     }
 });
 
-app.listen(8080, function () {
-    console.log('Example app listening on port 80!');
+app.listen(conf.port, function () {
+    console.log('Savas shuffle listening on port ' + conf.port);
 });
---
+++ conf.example.js
@@ -0,0 +1,5 @@
+var conf = {
+    port: 8080,
+    team: ['Anne', 'Chris', 'Dan', 'Kosta', 'Lisa', 'Oksana', 'Ro', 'Tim', 'Yonas'],
+    token: '',
+};
