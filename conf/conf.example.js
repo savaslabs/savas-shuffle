0bd6c61038d021da9051f70161de6b0eeb3944a3
Tim Stallmann <tim@stallmann.mayfirst.org>
2016-08-18 16:34:48 -0400

    Use single-word names

--- conf/conf.example.js
+++ conf/conf.example.js
@@ -1,6 +1,7 @@
 var conf = {
-    team: ['Someone', 'Someone else'],
-    moffice: ['Someone', 'Someone else'],
+    // Names need to be single words.
+    team: ['Someone', 'Someone-else', 'A-third-person'],
+    moffice: ['Someone', 'Someone-else', 'Person-Four'],
     token: 'test',
     meeting_channel: 'monday-meeting',
     port: 8080,
